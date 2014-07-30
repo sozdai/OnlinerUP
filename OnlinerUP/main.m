554322963276d8d4a746779465a6d39aa3c02a55
Alex Kardash <alexkardash@Alexs-MacBook-Air.local>
2014-07-30 18:15:17 +0300

    Initial Commit

---
+++ OnlinerUP/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OnlinerUP
+//
+//  Created by Alex Kardash on 7/30/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OnlinerUPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OnlinerUPAppDelegate class]));
+    }
+}
